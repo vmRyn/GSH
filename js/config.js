4a7ccdb1361683b30823f01a2f0f97e54a56df42
Ryn <31045220+vmRyn@users.noreply.github.com>
2021-03-11 23:52:51 +0000

    Finished, NEED TO FIX URLS.

---
+++ js/config.js
@@ -0,0 +1,25 @@
+var config = {
+    "text": {
+        "WebsiteTitle": "Coded By Ryn",
+        "Server1Name": "Community Teamspeak",
+        "Server2Name": "Community Discord",
+        "Server3Name": "Community Forums",
+        "Server4Name": "Community Applications"
+    },
+
+    "pictures": {
+        "WebsiteIcon": "../my-GameServers/images/Logo.png",
+        "CommunityLogo": "../my-GameServers/images/Forums.png",
+        "Server1Picture": "../my-GameServers/images/Teamspeak.png",
+        "Server2Picture": "../my-GameServers/images/Discord.png",
+        "Server3Picture": "../my-GameServers/images/Forum.png",
+        "Server4Picture": "../my-GameServers/images/Forms.png"
+    },
+
+    "urls": {
+        "Server1Link": "www.teamspeak.com",
+        "Server2Link": "www.teamspeak.com",
+        "Server3Link": "www.teamspeak.com",
+        "Server4Link": "www.teamspeak.com"
+    }
+}
---
+++ js/elements.js
@@ -0,0 +1,24 @@
+//Website Title
+document.getElementById('WebsiteTitle').innerHTML = config.text.WebsiteTitle;
+document.getElementById("WebsiteIcon").src = config.pictures.WebsiteIcon;
+
+//Block Names
+document.getElementById('Server1Name').innerHTML = config.text.Server1Name;
+document.getElementById('Server2Name').innerHTML = config.text.Server2Name;
+document.getElementById('Server3Name').innerHTML = config.text.Server3Name;
+document.getElementById('Server4Name').innerHTML = config.text.Server4Name;
+
+//Block Pictures
+document.getElementById("CommunityLogo").src = config.pictures.CommunityLogo;
+document.getElementById("Server1Picture").src = config.pictures.Server1Picture;
+document.getElementById("Server2Picture").src = config.pictures.Server2Picture;
+document.getElementById("Server3Picture").src = config.pictures.Server3Picture;
+document.getElementById("Server4Picture").src = config.pictures.Server4Picture;
+
+//Links
+document.getElementById("Server1Link").href = "https://facebook.com"
+document.getElementById("Server2Link").href = config.urls.Server2Link;
+document.getElementById("Server3Link").href = config.urls.Server3Link;
+document.getElementById("Server4Link").href = config.urls.Server4Link;
+
+//Important Code
